8e99ccd29ffffb1ce899e2e32cee67b8a1c448b4
Mahendra Sainath Reddy Chintakunta <mahendrasainathreddy8@gmail.com>
2024-01-03 21:15:43 +0530

    Day 1

---
+++ src/com/yt1/CarDetails.java
@@ -0,0 +1,17 @@
+package com.yt1;
+
+public class CarDetails {
+    public static void main(String[] args) {
+        String name = "Fortuner";
+        int price = 4000000;
+        double weight = 109.45;
+        char logo = 'F';
+        boolean available = true;
+
+        System.out.println(name);
+        System.out.println(price);
+        System.out.println(weight);
+        System.out.println(logo);
+        System.out.println(available);
+    }
+}
